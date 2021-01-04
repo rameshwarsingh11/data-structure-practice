d6b62964bc7768c113f38b3ab15d9380cdc87a96
Rameshwar Singh <rameshwar.job2@gmail.com>
2021-01-04 11:51:16 -0500

    print DayLight as per instructions

---
+++ print_DayLight.py
@@ -0,0 +1,12 @@
+# Program to print integers from 1 to 100. Print 'Day' for multiple of 3 and 'Light' for multiple of 5. 
+# For integrers a multiple of both 3 and five, you should print 'DayLight'. In other cases, print the integer itself.
+
+for num in range(0,101):
+    if num%3 == 0 and num%5 ==0:
+        print('DayLight')
+    elif num%3 ==0:
+        print('Day')
+    elif num%5 == 0:
+        print('Light')
+    else:
+        print(num)
