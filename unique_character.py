09ec8f37bc10fca005427ae828e8e479ba9c54de
Rameshwar Singh <rameshwar.job2@gmail.com>
2021-01-05 16:30:34 -0500

    python code to check if a string has unique characters only

---
+++ unique_character.py
@@ -0,0 +1,19 @@
+# Problem : Find out if a string has all unique chracters.
+# Solution 1 : Using a built in data structure with function.
+def unique_character(string):
+  return len(set(string)) == len(string)
+
+print(unique_character('cfghiit'))
+
+# Solution 2 : Create a lookup function
+def unique_character2(string):
+  characters = set()
+  for letter in string:
+    if letter in characters:
+      return False
+    else:
+      characters.add(letter)
+
+  return True
+
+print(unique_character2('cfghit'))
