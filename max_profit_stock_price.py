7b6e4ec2b8d41acbe30fd46b58a1a845e0882c52
Rameshwar Singh <rameshwar.job2@gmail.com>
2021-04-15 10:54:59 -0400

    Create max_profit_stock_price.py
    Find the max profit in the given list of stock prices.

---
+++ max_profit_stock_price.py
@@ -0,0 +1,26 @@
+# Find the most beneficial transaction / max profit in the stock price list.
+# Use greedy algorithm approach.
+
+def profit(stock_prices):
+
+  # Check edge case : If the there are less than 2 stock prices given in the stock list
+  if len(stock_prices) < 2:
+    raise Exception(
+        'Sorry, at least two stock prices are required to compare.')
+
+  min_stock_price = stock_prices[0]
+
+  max_profit = 0
+
+  for price in stock_prices:
+    min_stock_price = min(min_stock_price, price)
+    # print('min_stock_price',min_stock_price)
+    comparison_profit = price - min_stock_price
+    # print('comparison_profit',comparison_profit)
+    max_profit = max(max_profit, comparison_profit)
+    # print('max_profit',max_profit)
+    # print('\n')
+  return max_profit
+
+
+print(profit([45, 60, 50, 10]))
