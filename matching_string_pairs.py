41a679449374c8ddb6bd5f37e79d67ca7ea769a9
Rameshwar Singh <rameshwar.job2@gmail.com>
2021-04-23 14:38:55 -0400

    Create matching_string_pairs.py
    Matching pairs in source and target strings.

---
+++ matching_string_pairs.py
@@ -0,0 +1,38 @@
+def matching_pairs(source, target):
+  # Checking edge case when source and target strings are same.
+  if source== target:
+    return len(source) - 2
+  
+  unmatched_pairs = set()
+  unmatched_in_target = set()
+  unmatched_in_source = set()
+  count = 0
+  found_perfect_swap = False
+  partial_swap = False
+
+  for i in range(len(source)):
+    if source[i] == target[i]:
+      count += 1
+    if found_perfect_swap:
+      # Skip looking for swap
+      continue
+
+    if source[i] != target[i]:
+      unmatched_pairs.add((source[i], target[i]))
+      unmatched_in_target.add(target[i])
+      unmatched_in_source.add(source[i])
+ 
+      if (target[i], source[i]) in unmatched_pairs:
+        found_perfect_swap = True
+
+      elif source[i] in unmatched_in_target or target[i] in unmatched_in_source:
+        partial_swap = True
+
+  if found_perfect_swap:
+    return count + 2
+  elif partial_swap:
+    return count + 1
+
+print(matching_pairs(('abcd'),('adcb')))
+print(matching_pairs(('aaaa'),('aaaa')))
+print(matching_pairs(('mno'),('nmo')))
