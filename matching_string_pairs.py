11080039e08738365705efd4e2e15a9b80c4ab36
Rameshwar Singh <rameshwar.job2@gmail.com>
2021-05-20 17:56:13 -0400

    Update matching_string_pairs.py
    Added simple test cases.

--- matching_string_pairs.py
+++ matching_string_pairs.py
@@ -1,8 +1,8 @@
 def matching_pairs(source, target):
   # Checking edge case when source and target strings are same.
-  if source== target:
+  if source == target:
     return len(source) - 2
-  
+
   unmatched_pairs = set()
   unmatched_in_target = set()
   unmatched_in_source = set()
@@ -21,7 +21,7 @@ def matching_pairs(source, target):
       unmatched_pairs.add((source[i], target[i]))
       unmatched_in_target.add(target[i])
       unmatched_in_source.add(source[i])
- 
+
       if (target[i], source[i]) in unmatched_pairs:
         found_perfect_swap = True
 
@@ -33,6 +33,28 @@ def matching_pairs(source, target):
   elif partial_swap:
     return count + 1
 
-print(matching_pairs(('abcd'),('adcb')))
-print(matching_pairs(('aaaa'),('aaaa')))
-print(matching_pairs(('mno'),('nmo')))
+
+def check(expected, output):
+  rightTick = '\u2713'
+  wrongTick = '\u2717'
+
+  if expected == output:
+    print(rightTick, 'Test passed!')
+
+  else:
+    print(wrongTick, 'Test failed.')
+
+
+if __name__ == '__main__':
+  expected_1 = 4
+  expected_2 = 2
+  expected_3 = 3
+  output_1 = matching_pairs(('abcd'), ('adcb'))
+  check(expected_1, output_1)
+  output_2 = matching_pairs(('aaaa'), ('aaaa'))
+  check(expected_2, output_2)
+  output_3 = matching_pairs(('mno'), ('nmo'))
+  check(expected_3, output_3)
+  print(output_1)
+  print(output_2)
+  print(output_3)
