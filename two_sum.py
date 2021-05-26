74d573d05601871a948ec6158c33902f33aba82f
Rameshwar Singh <rameshwar.job2@gmail.com>
2021-05-26 17:40:20 -0400

    Update two_sum.py
    Added a simple test case.

--- two_sum.py
+++ two_sum.py
@@ -14,7 +14,7 @@ def two_sum(list, target):
   return False
 
 
-print(two_sum([3, 6, 5], 9))
+#print(two_sum([3, 6, 5], 9))
 
 
 # Another efficient solution :
@@ -22,15 +22,29 @@ def twoSum(arr, target):
       seen = {}
       for i, v in enumerate(arr):
             remaining = target - v
-            print(remaining)
+            #print(remaining)
             if remaining in seen:
-                print('seen[remaining]', seen[remaining])
+                #print('seen[remaining]', seen[remaining])
                 return [seen[remaining], i]
             seen[v] = i
-            print('seen', seen)
+            #print('seen', seen)
       return []
 
 
-target = 6
-arr = [1, 2, 3,4]
-print(twoSum(arr, target))
+def check(expected, output):
+  rightTick = '\u2713'
+  wrongTick = '\u2717'
+  if expected == output:
+    print(rightTick, 'Test passed!')
+  else:
+    print(wrongTick, 'Test failed.')
+
+if __name__ == '__main__':
+  target = 6
+  arr = [1, 2, 3,4]
+  expected = [1,3]
+  output = twoSum(arr, target)
+  check(expected, output)
+  print(output)
+
+
