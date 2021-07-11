f648c77ed2679dffe5a5be9e1542ec6029d1c2ef
Rameshwar Singh <rameshwar.job2@gmail.com>
2021-07-11 10:51:43 -0400

    Update print_DayLight.py
    Refactored.

--- print_DayLight.py
+++ print_DayLight.py
@@ -1,10 +1,15 @@
 # Program to print integers from 1 to 100. Print 'Day' for multiple of 3 and 'Light' for multiple of 5.
 # For integers a multiple of both 3 and five, you should print 'DayLight'. In other cases, print the integer itself.
 
-for num in range(0, 10):
-    if num % 3 == 0 and num % 5 == 0:
-        print('DayLight')
-    elif num % 3 == 0:
-        print('Day')
-    elif num % 5 == 0:
-        print('Light')
+def printDayLight():
+    for num in range(0, 10):
+        if num % 3 == 0 and num % 5 == 0:
+            print('DayLight')
+        elif num % 3 == 0:
+            print('Day')
+        elif num % 5 == 0:
+            print('Light')
+
+if __name__ == '__main__':
+    printDayLight()
+
